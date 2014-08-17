d1d75f73f7e8d675fa6234fb0b19282db3977649
mayko <mayko_90@hotmail.com>
2014-08-17 06:29:06 -0300

    soma

---
+++ teste3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main(){
+int numero= 10, soma=0;
+
+while (numero != 0){
+printf("digite um numero: ");
+scanf("%d", &numero);
+soma= soma+numero;
+}
+printf("a soma dos valores e : %d", soma);
+
+
+}
